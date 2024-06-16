3692579e20e170e92f6cd34d22d3f50245c8f7a0
Tico Steenbrink <tico_steenbrink@hotmail.com>
2024-06-16 11:41:06 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,19 @@
+prijzen = {
+    "aardbei"   : 3,
+    "vanille"   : 4,
+    "chocolade" : 5,
+}
+
+aanbieding = prijzen["aardbei"] * 0.8
+
+reclame_tekst  = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
+reclame_tekst2 = reclame_tekst[0:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+
+mijn_string = reclame_tekst4
+for el in mijn_string:
+    if len(el) >= 5:
+        print(el.upper())
+    else:
+        print(el.lower())
