e919a28802dbe84e7182d7fad4b4dfc72655e01f
Tico Steenbrink <tico_steenbrink@hotmail.com>
2024-06-24 11:22:12 +0200

    1 regel toegevoegd

--- helper.py
+++ helper.py
@@ -7,13 +7,15 @@ def decoreer(tekst=""):
     print()
 
 def fooi_pp(bedrag, personen):
-    try:
-        bedrag_pp = bedrag/personen
-    except:
-        bedrag_pp = "??"
+    bedrag_pp = bedrag/personen
     return f"Het bedrag per persoon is {bedrag_pp} euro"
 
-b = int(input("Welk bedrag zit er in de fooienpot? "))
-p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
 
-print(fooi_pp(b,p))
+
+def som(inkomsten):
+    return sum(inkomsten.values())
